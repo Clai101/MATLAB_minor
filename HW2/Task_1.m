4559f6479b80e5fc09427db43ea1d984336a8d08
Clai101 <75207272+Clai101@users.noreply.github.com>
2022-10-20 12:11:37 +0300

    Matvei

--- HW2/Task_1.m
+++ HW2/Task_1.m
@@ -1,4 +1,4 @@
-%% Строим графики
+%% Строим изолинии arg  and  abs
 k = 1; m = 15;
 [x,y] = meshgrid(linspace( -0.4, 0.4, 3000), linspace(-0.4, 0.4 ,3000));
 z = complex(x,y);
@@ -18,7 +18,58 @@ hold on;
 %Выводим графики
 contour(x, y, abs(Q(z)),[0, 1, 2, 3, 4, 5, 10], 'ShowText', 'on', Color = 'blue');
 hold on;
-contour(x, y, angle(Q(z)), 'ShowText', 'on', Color = 'red');
+contour(x, y, angle(Q(z)), [0,  pi /6,  pi /3, pi/2, pi], 'ShowText', 'on', Color = 'red');
 hold on;
 %Создаем легенду
-legend('$Re \{z^3 + imz^2 - kz + 1\}$','$Im \{z^3 + imz^2 - kz + 1\}$', Interpreter = 'latex', FontSize = 10)
+legend('$\left| \frac{1}{z^3 + imz^2 - kz + 1} \right| $',['$Arg \{  \frac{1}{z^3 + imz^2 - kz + 1}\}$'], Interpreter = 'latex', FontSize = 10)
+
+%% Доп корень
+k = 1; m = 15;
+[x,y] = meshgrid(linspace( -0.1, 0.1, 3000), linspace(-14.8, -15 ,3000));
+z = complex(x,y);
+
+% Определяем функцию и облась
+Q = @(z)  1./(z.^3 + 15i*z.^2 - z.*k + 1);
+
+%Создаем рамку
+figure(2);
+xlabel('$Re(z)$', Interpreter = 'latex', FontSize = 14);
+ylabel('$Im(z)$', Interpreter = 'latex', FontSize = 14);
+grid on;
+set (gca, "xaxislocation", "origin")
+set (gca, "yaxislocation", "origin")
+hold on;
+
+%Выводим графики
+contour(x, y, abs(Q(z)), [0, 0.05,  0.1, 0.15, 0.4, 1, 2, 3], 'ShowText', 'on', Color = 'blue');
+hold on;
+contour(x, y, angle(Q(z)), [0, pi/6, pi/3 ,pi/2, pi], 'ShowText', 'on', Color = 'red');
+hold on;
+%Создаем легенду
+legend('$ \left| \frac{1}{z^3 + imz^2 - kz + 1} \right| $','$Arg \{  \frac{1}{z^3 + imz^2 - kz + 1}\}$', Interpreter = 'latex', FontSize = 10)
+
+
+%% Анализ
+k = 1; m = 15;
+[x,y] = meshgrid(linspace( -12, 12, 3000), linspace(-20, 4 ,3000));
+z = complex(x,y);
+
+% Определяем функцию и облась
+Q = @(z)  1./(z.^3 + 15i*z.^2 - z.*k + 1);
+
+%Создаем рамку
+figure(3);
+xlabel('$Re(z)$', Interpreter = 'latex', FontSize = 14);
+ylabel('$Im(z)$', Interpreter = 'latex', FontSize = 14);
+grid on;
+set (gca, "xaxislocation", "origin")
+set (gca, "yaxislocation", "origin")
+hold on;
+
+%Выводим графики
+contour(x, y, real(Q(z)),[0, 1, 2, 3, 4, 5, 10], 'ShowText', 'on', Color = 'blue');
+hold on;
+contour(x, y, imag(Q(z)), 'ShowText', 'on', Color = 'red');
+hold on;
+%Создаем легенду
+legend('$Re \{  \frac{1}{z^3 + imz^2 - kz + 1}\}$','$Im \{ \frac{1}{z^3 + imz^2 - kz + 1}\}$', Interpreter = 'latex', FontSize = 10)
--- HW2/Task_2.m
+++ HW2/Task_2.m
@@ -1,11 +1,12 @@
-%% Строим графики
-k = 1; m = 15;
-x = linspace( -0, 1, 3000);
+%Константы
+k = 15; m = 1;
+p = linspace( -2,2, 100);
+% Зависимости
+transformed = @(x) imag(w(f(x)));
+f = @(x) complex(x, x.*k*(-1)^m);
+w = @(z) 1/2 * (z + 1/z);
 
-% Определяем функцию и облась
-f = @(x)  x.^2;
-
-%Создаем рамку
+%Окно и настройки
 figure(1);
 xlabel('$Re(z)$', Interpreter = 'latex', FontSize = 14);
 ylabel('$Im(z)$', Interpreter = 'latex', FontSize = 14);
@@ -13,10 +14,17 @@ grid on;
 set (gca, "xaxislocation", "origin")
 set (gca, "yaxislocation", "origin")
 hold on;
-
-%Выводим графики
-fplot(f);
+axis([-2 2 -100 100])
 hold on;
 
-%Создаем легенду
-legend('$Re \{z^3 + imz^2 - kz + 1\}$','$Im \{z^3 + imz^2 - kz + 1\}$', Interpreter = 'latex', FontSize = 10)
+%Графики
+plot(p, imag(f(p)),  'Linewidth',1, Color="red")
+hold on;
+fplot(transformed, [-2, -0.0001], 'Linewidth',1, Color="blue")
+hold on;
+fplot(transformed, [0.0001, 2 ],  'Linewidth',1, Color="blue")
+hold on;
+
+%Легенда
+legend('$xk(-1)^m$','$ Transforme $', Interpreter = 'latex', FontSize = 10)
+hold off;
