d860677a377ef025c5b25ba8503d2f7a506e9dbe
Clai101 <75207272+Clai101@users.noreply.github.com>
2022-10-21 08:48:21 +0300

    Matvei

--- HW2/Task_1.m
+++ HW2/Task_1.m
@@ -15,6 +15,7 @@ set (gca, "xaxislocation", "origin")
 set (gca, "yaxislocation", "origin")
 hold on;
 
+
 %Выводим графики
 contour(x, y, abs(Q(z)),[0, 1, 2, 3, 4, 5, 10], 'ShowText', 'on', Color = 'blue');
 hold on;
