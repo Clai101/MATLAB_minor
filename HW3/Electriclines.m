db0b59d0908d8c0abb3ba298f757e0ebfd472c47
Clai101 <75207272+Clai101@users.noreply.github.com>
2022-11-11 10:29:37 +0300

    Matvei

--- HW3/Electriclines.m
+++ HW3/Electriclines.m
@@ -2,5 +2,6 @@ D = 1;
 [z, x, y] = meshgrid(linspace(-10, 10, 500), linspace(-10, 10, 500), linspace(-10, 10, 500));
 phi = (D* 0.25*(z.^2 + y.^2 + x.^2).^(-3/2) ) .*  (3 * (1 + ((x + y)./z).^2).^(-1)- 1);
 [u, v, w] = gradient(phi);
+
 l = streamline(stream3(x,y,z,u,v,w,10,10,10));
 set(l,'Color','k');
